9828129a9d51209e4c74dae4316a2635f58f0356
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-05-07 17:53:38 +0500

    cpupon wali bakchodi

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
+import customerRouter from "./customer/cusotmerRouter";
+import couponRouter from "./coupon/couponRouter";
 
 const app = express();
 app.use(cookieParser());
@@ -9,6 +11,9 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from order service service!" });
 });
+
+app.use("/customer", customerRouter);
+app.use("/coupons", couponRouter);
 
 app.use(globalErrorHandler);
 
