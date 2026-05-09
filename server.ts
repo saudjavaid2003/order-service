f9f05da9822a5e0c9a35014466cb9a030a5d7132
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-05-09 22:31:40 +0500

    consuming messages from catalog-service

--- server.ts
+++ server.ts
@@ -2,12 +2,20 @@ import app from "./src/app";
 import config from "config";
 import logger from "./src/config/logger";
 import connectDB from "./src/config/db";
+import { MessageBroker } from "./src/types/broker";
+import { createMessageBroker } from "./src/common/factories/brokerFactory";
 
 const startServer = async () => {
   const PORT = config.get("server.port") || 5503;
+  let broker: MessageBroker | null = null;
 
   try {
     await connectDB();
+    broker = createMessageBroker();
+    // await broker.connectProducer();
+
+    await broker.connectConsumer();
+    await broker.consumeMessage(["product", "topping"], false);
 
     app
       .listen(PORT, () => console.log(`Listening on port ${PORT}`))
@@ -17,6 +25,10 @@ const startServer = async () => {
       });
   } catch (err) {
     logger.error("Error happened: ", err.message);
+    if (broker) {
+      // await broker.disconnectProducer();
+      await broker.disconnectConsumer();
+    }
     process.exit(1);
   }
 };
---
+++ src/config/kafka.ts
@@ -0,0 +1,114 @@
+import {
+  Consumer,
+  EachMessagePayload,
+  Kafka,
+  KafkaConfig,
+} from "kafkajs";
+import { MessageBroker } from "../types/broker";
+
+export class KafkaBroker implements MessageBroker {
+  private consumer: Consumer;
+  // private producer: Producer;
+
+  constructor(clientId: string, brokers: string[]) {
+    const  kafkaConfig: KafkaConfig = {
+      clientId,
+      brokers,
+    };
+    // if (process.env.NODE_ENV === "production") {
+    //   kafkaConfig = {
+    //     ...kafkaConfig,
+    //     ssl: config.get("kafka.ssl"),
+    //     connectionTimeout: 45000,
+    //     sasl: {
+    //       mechanism: "plain",
+    //       username: config.get("kafka.sasl.username"),
+    //       password: config.get("kafka.sasl.password"),
+    //     },
+    //   };
+    // }
+
+    const kafka = new Kafka(kafkaConfig);
+
+    // this.producer = kafka.producer();
+    this.consumer = kafka.consumer({ groupId: clientId });
+  }
+
+  /**
+   * Connect the consumer
+   */
+  async connectConsumer() {
+    await this.consumer.connect();
+  }
+
+  // /**
+  //  * Connect the producer
+  //  */
+  // async connectProducer() {
+  //   await this.producer.connect();
+  // }
+
+  /**
+   * Disconnect the consumer
+   */
+  async disconnectConsumer() {
+    await this.consumer.disconnect();
+  }
+
+  // /**
+  //  * Disconnect the producer
+  //  */
+  // async disconnectProducer() {
+  //   if (this.producer) {
+  //     await this.producer.disconnect();
+  //   }
+  // }
+
+  // /**
+  //  * @param topic - the topic to send the message to
+  //  * @param message - The message to send
+  //  * @throws {Error} - When the producer is not connected
+  //  */
+  // async sendMessage(topic: string, message: string, key: string) {
+  //   const data: { value: string; key?: string } = {
+  //     value: message,
+  //   };
+
+  //   if (key) {
+  //     data.key = key;
+  //   }
+
+  //   await this.producer.send({
+  //     topic,
+  //     messages: [data],
+  //   });
+  // }
+
+  async consumeMessage(topics: string[], fromBeginning: boolean = false) {
+    await this.consumer.subscribe({ topics, fromBeginning });
+
+    await this.consumer.run({
+      eachMessage: async ({
+        topic,
+        partition,
+        message,
+      }: EachMessagePayload) => {
+        console.log({
+          value: message.value.toString(),
+          topic,
+          partition,
+        });
+        // switch (topic) {
+        //   case "product":
+        //     await handleProductUpdate(message.value.toString());
+        //     return;
+        //   case "topping":
+        //     await handleToppingUpdate(message.value.toString());
+        //     return;
+        //   default:
+        //     console.log("Doing nothing...");
+        // }
+      },
+    });
+  }
+}
