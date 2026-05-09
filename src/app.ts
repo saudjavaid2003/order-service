5908e37cd133ab3eafc869ef22f05b5daf062b2b
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-05-10 01:56:40 +0500

    caltuing subtotal making order applying taxes

--- src/app.ts
+++ src/app.ts
@@ -3,29 +3,19 @@ import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./customer/cusotmerRouter";
 import couponRouter from "./coupon/couponRouter";
-import cors from "cors";
-import  config from "config";
+import orderRouter from "./order/orderRouter";
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-const ALLOWED_DOMAINS = [
-    config.get("frontend.clientUi"),
-];
-
-app.use(
-    cors({
-        origin: ALLOWED_DOMAINS as string[],
-        credentials: true,
-    }),
-);
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from order service service!" });
 });
 
 app.use("/customer", customerRouter);
 app.use("/coupons", couponRouter);
+app.use("/orders", orderRouter);
 
 app.use(globalErrorHandler);
 
---
+++ src/order/orderController.ts
@@ -0,0 +1,144 @@
+import { Request, Response } from "express";
+import {
+  CartItem,
+  ProductPricingCache,
+  Topping,
+  ToppingPriceCache,
+} from "../types";
+import productCacheModel from "../productCache/productCacheModel";
+import toppingCacheModel from "../topppingCache/toppingCacheModel";
+import couponModel from "../coupon/couponModel";
+
+export class OrderController {
+  create = async (req: Request, res: Response) => {
+    // todo: validate request data.
+
+    const totalPrice = await this.calculateTotal(req.body.cart);
+
+    let discountPercentage = 0;
+
+    const couponCode = req.body.couponCode;
+    const tenantId = req.body.tenantId;
+
+    if (couponCode) {
+      discountPercentage = await this.getDiscountPercentage(
+        couponCode,
+        tenantId,
+      );
+    }
+
+    const discountAmount = Math.round((totalPrice * discountPercentage) / 100);
+
+    const priceAfterDiscount = totalPrice - discountAmount;
+
+    // todo: May be store in db for each tenant.
+    const TAXES_PERCENT = 18;
+
+    const taxes = Math.round((priceAfterDiscount * TAXES_PERCENT) / 100);
+
+    return res.json({ taxes: taxes });
+  };
+
+  private calculateTotal = async (cart: CartItem[]) => {
+    const productIds = cart.map((item) => item._id);
+
+    // todo: proper error handling..
+    const productPricings = await productCacheModel.find({
+      productId: {
+        $in: productIds,
+      },
+    });
+
+    // todo: What will happen if product does not exists in the cache
+    // 1. call catalog service.
+    // 2. Use price from cart <- BAD
+
+    const cartToppingIds = cart.reduce((acc, item) => {
+      return [
+        ...acc,
+        ...item.chosenConfiguration.selectedToppings.map(
+          (topping) => topping.id,
+        ),
+      ];
+    }, []);
+
+    // todo: What will happen if topping does not exists in the cache
+    const toppingPricings = await toppingCacheModel.find({
+      toppingId: {
+        $in: cartToppingIds,
+      },
+    });
+
+    const totalPrice = cart.reduce((acc, curr) => {
+      const cachedProductPrice = productPricings.find(
+        (product) => product.productId === curr._id,
+      );
+
+      return (
+        acc +
+        curr.qty * this.getItemTotal(curr, cachedProductPrice, toppingPricings)
+      );
+    }, 0);
+
+    return totalPrice;
+  };
+
+  private getItemTotal = (
+    item: CartItem,
+    cachedProductPrice: ProductPricingCache,
+    toppingsPricings: ToppingPriceCache[],
+  ) => {
+    const toppingsTotal = item.chosenConfiguration.selectedToppings.reduce(
+      (acc, curr) => {
+        return acc + this.getCurrentToppingPrice(curr, toppingsPricings);
+      },
+      0,
+    );
+
+    const productTotal = Object.entries(
+      item.chosenConfiguration.priceConfiguration,
+    ).reduce((acc, [key, value]) => {
+      const price =
+        cachedProductPrice.priceConfiguration[key].availableOptions[value];
+      return acc + price;
+    }, 0);
+
+    return productTotal + toppingsTotal;
+  };
+
+  private getCurrentToppingPrice = (
+    topping: Topping,
+    toppingPricings: ToppingPriceCache[],
+  ) => {
+    const currentTopping = toppingPricings.find(
+      (current) => topping.id === current.toppingId,
+    );
+
+    if (!currentTopping) {
+      // todo: Make sure the item is in the cache else, maybe call catalog service.
+      return topping.price;
+    }
+
+    return currentTopping.price;
+  };
+
+  private getDiscountPercentage = async (
+    couponCode: string,
+    tenantId: string,
+  ) => {
+    const code = await couponModel.findOne({ code: couponCode, tenantId });
+
+    if (!code) {
+      return 0;
+    }
+
+    const currentDate = new Date();
+    const couponDate = new Date(code.validUpto);
+
+    if (currentDate <= couponDate) {
+      return code.discount;
+    }
+
+    return 0;
+  };
+}
