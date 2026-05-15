6252c161560a97c9dc360951b88c3b5a607d052d
saudjavaid2003 <saudjavaid2003@users.noreply.github.com>
2026-05-15 05:31:19 +0500

    add status change endppoint for the admins and managers

--- src/order/orderController.ts
+++ src/order/orderController.ts
@@ -3,6 +3,7 @@ import { Request as AuthRequest } from "express-jwt";
 import {
   CartItem,
   ProductPricingCache,
+  ROLES,
   Topping,
   ToppingPriceCache,
 } from "../types";
@@ -133,6 +134,43 @@ export class OrderController {
     return res.json({ paymentUrl: null });
   };
 
+  getAll = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const { role, tenant: userTenantId } = req.auth;
+
+    const tenantId = req.query.tenantId;
+
+    if (role === ROLES.CUSTOMER) {
+      return next(createHttpError(403, "Not allowed."));
+    }
+
+    if (role === ROLES.ADMIN) {
+      const filter = {};
+
+      if (tenantId) {
+        filter["tenantId"] = tenantId;
+      }
+
+      // todo: VERY IMPORTANT. add pagination.
+      const orders = await orderModel
+        .find(filter, {}, { sort: { createdAt: -1 } })
+        .populate("customerId")
+        .exec();
+
+      // todo: add logger
+      return res.json(orders);
+    }
+
+    if (role === ROLES.MANAGER) {
+      const orders = await orderModel
+        .find({ tenantId: userTenantId }, {}, { sort: { createdAt: -1 } })
+        .populate("customerId")
+        .exec();
+
+      return res.json(orders);
+    }
+
+    return next(createHttpError(403, "Not allowed."));
+  };
   getMine = async (req: AuthRequest, res: Response, next: NextFunction) => {
     const userId = req.auth.sub;
 
@@ -208,6 +246,41 @@ export class OrderController {
     }
 
     return next(createHttpError(403, "Operation not permitted."));
+  };
+
+  changeStatus = async (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const { role, tenant: tenantId } = req.auth;
+    const orderId = req.params.orderId;
+
+    if (role === ROLES.MANAGER || ROLES.ADMIN) {
+      const order = await orderModel.findOne({ _id: orderId });
+      if (!order) {
+        return next(createHttpError(400, "Order not found."));
+      }
+
+      const isMyRestaurantOrder = order.tenantId === tenantId;
+
+      if (role === ROLES.MANAGER && !isMyRestaurantOrder) {
+        return next(createHttpError(403, "Not allowed."));
+      }
+
+      const updatedOrder = await orderModel.findOneAndUpdate(
+        { _id: orderId },
+        // todo: req.body.status <- Put proper validation.
+        { orderStatus: req.body.status },
+        { new: true },
+      );
+
+      // todo: send to kafka
+
+      return res.json({ _id: updatedOrder._id });
+    }
+
+    return next(createHttpError(403, "Not allowed."));
   };
 
   private calculateTotal = async (cart: CartItem[]) => {
--- src/order/orderRouter.ts
+++ src/order/orderRouter.ts
@@ -1,3 +1,4 @@
+
 import express from "express";
 import authenticate from "../common/middleware/authenticate";
 import { asyncWrapper } from "../utils";
@@ -12,7 +13,13 @@ const broker = createMessageBroker();
 const orderController = new OrderController(paymentGw, broker);
 
 router.post("/", authenticate, asyncWrapper(orderController.create));
+router.get("/", authenticate, asyncWrapper(orderController.getAll));
 router.get("/mine", authenticate, asyncWrapper(orderController.getMine));
 router.get("/:orderId", authenticate, asyncWrapper(orderController.getSingle));
+router.patch(
+  "/change-status/:orderId",
+  authenticate,
+  asyncWrapper(orderController.changeStatus),
+);
 
 export default router;
